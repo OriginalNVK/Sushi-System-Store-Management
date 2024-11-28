327b34b339cc7268fe82909ae23340434730a7db
Red <leviethung1792k4@gmail.com>
2024-11-28 20:00:22 +0700

    demo api

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,11 +1,37 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+const app = express();
+app.use(cors());
+
+const config = {
+    user: "sa",
+    password: "1303",
+    server: "DESKTOP-EKEQ0IF\\SQLEXPRESS",
+    database: "QUANLINHAHANGSUSHI",
+    options: {
+        trustServerCertificate: true,
+        enableArithAbort: true,
+    },
+    port: 1433
+};
+
+app.get('/', (req, res) => {
+    return res.json("Hi, I am back");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/area', async function (req, res) {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request().query("SELECT * FROM AREA");
+        return res.json(result.recordset);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "An error occurred" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
