5ec1f0416480dda8881cf968e162b4dec6e4009d
danghuy0104 <134122759+danghuy0104@users.noreply.github.com>
2024-12-18 16:18:54 +0700

    Delete Back-end/routes directory

--- Back-end/routes/customerRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllCustomers } = require("../controllers/customerController");
-
-router.get("/", getAllCustomers);
-
-module.exports = router;
