1f6dada34a7618b870efa476bee5facc9bdf588f
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2024-12-23 23:42:32 +0700

    Delete the file dbConfig

---
+++ Back-end/controllers/departmentController.js
@@ -0,0 +1,14 @@
+const {
+    getDepartmentModel
+} = require("../models/departmentModel");
+
+const getDepartmentController = async (req, res) => {
+    try {
+        const departments = await getDepartmentModel();
+        res.json(departments);
+    }
+    catch(err) {
+        console.error("Error fetching departments:", err);
+        res.status(500).json({ error: "An error occurred while fetching departments" });
+    }
+}
