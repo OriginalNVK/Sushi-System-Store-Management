5196f93dc47c69cfc578f4d379a64e3f6f641d67
danghuy0104 <134122759+danghuy0104@users.noreply.github.com>
2024-12-29 17:24:18 +0700

    api report

---
+++ Back-end/routes/reportRevenueRouter.js
@@ -0,0 +1,31 @@
+// routes/reportRevenueRouter.js
+
+const express = require('express');
+const router = express.Router();
+const reportRevenueController = require('../controllers/reportRevenueController');
+
+// Tổng quan doanh thu theo ngày
+router.post('/overview/date', reportRevenueController.getRevenueOverviewByDate);
+
+// Chi tiết doanh thu theo ngày
+router.post('/detail/date', reportRevenueController.getReportDetailByDate);
+
+// Tổng quan doanh thu theo tháng
+router.post('/overview/month', reportRevenueController.getRevenueOverviewByMonth);
+
+// Chi tiết doanh thu theo tháng
+router.post('/detail/month', reportRevenueController.getReportDetailByMonth);
+
+// Tổng quan doanh thu theo quý
+router.post('/overview/quarter', reportRevenueController.getRevenueOverviewByQuarter);
+
+// Chi tiết doanh thu theo quý
+router.post('/detail/quarter', reportRevenueController.getReportDetailByQuarter);
+
+// Tổng quan doanh thu theo năm
+router.post('/overview/year', reportRevenueController.getRevenueOverviewByYear);
+
+// Chi tiết doanh thu theo năm
+router.post('/detail/year', reportRevenueController.getReportDetailByYear);
+
+module.exports = router;
